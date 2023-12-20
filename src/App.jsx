ad734ae02db42f33811a99f3749aac91f480c68e
Harshit Sharma <harshits988@gmail.com>
2023-12-20 17:27:37 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import './styles.scss';
 import { useState } from 'react';
 import Board from './components/Board';
+import StatusMessage from './components/StatusMessage';
 import { calculateWinner } from './winner';
+
 function App() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [isXNext, setIsXNext] = useState(false);
   const winner = calculateWinner(squares);
-  const nextPlayer = isXNext ? 'X' : '0';
-  const statusMessage = winner ? `Winner is ${winner}` : `Next Player is ${nextPlayer}`
+
+
 
   const handleSquareClick = clickedPosition => {
 
@@ -30,7 +32,8 @@ function App() {
   return (
 
     <div className='app'>
-      <h2>{statusMessage}</h2>
+
+      <StatusMessage winner={winner} isXNext={isXNext} squares={squares} />
       <Board squares={squares} handleSquareClick={handleSquareClick} />
     </div>
   );
---
+++ src/components/StatusMessage.jsx
@@ -0,0 +1,44 @@
+
+// eslint-disable-next-line react/prop-types
+const StatusMessage = ({ winner, isXNext, squares }) => {
+
+
+    // eslint-disable-next-line react/prop-types
+    const noMovesLeft = squares.every(squareValue => squareValue !== null);
+    const nextPlayer = isXNext ? 'X' : '0';
+
+    const renderStatusMessage = () => {
+        if (winner) {
+            return (
+                <>
+                    Winner is {' '}
+                    <span className={winner === 'X' ? 'text-green' : 'text-orange'}>
+                        {winner}
+                    </span>
+                </>
+            );
+        }
+        if (!winner && noMovesLeft) {
+            return <><span className="text-orange">0</span> and{' '}
+                <span className="text-green">X</span> tied
+            </>
+        }
+        if (!winner && !noMovesLeft) {
+            return (
+                <>
+                    Next player is {' '}
+                    <span className={isXNext ? 'text-green' : 'text-orange'}>
+                        {nextPlayer}
+                    </span>
+                </>
+            );
+        }
+        return null;
+    };
+    return (
+        <h2 className="status-message">
+            {renderStatusMessage()}
+        </h2>
+    );
+};
+export default StatusMessage;
